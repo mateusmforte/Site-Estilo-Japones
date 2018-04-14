501d968d1dd664a66454a288fca3924e9e7a951b
Mateus Moura Forte <m-forte@hotmail.com>
2018-04-14 14:15:28 -0300

    Animação do Cardápio finalizada
    Removida a classe .opcoes-menu que não estava sendo utilizada
    
    Removida a propriedade Repeat(inválida) do background das section "Onde estamos"

--- assets/js/cardapio.js
+++ assets/js/cardapio.js
@@ -1,6 +1,5 @@
 var menu = document.querySelector('#menu');
-var botaoVoltar = document.querySelector('#botaoVoltar');
-var textoOpcoes = document.querySelector('#texto-opcoes');
+var botaoVoltar = document.querySelectorAll(".botaoVoltar");
 function mostraOpcoesSabor(event){
    var opcaoEscolhida = event.target.id;
    var opcoesDisponiveis = ["opcao-temaki",
@@ -19,20 +18,35 @@ function mostraOpcoesSabor(event){
                         "#yakisoba",
                         "#variados",
                         "#bebidas"];
-   console.log(opcaoEscolhida);
    for(var i = 0; i<opcoesDisponiveis.length; i++){
        if(opcaoEscolhida == opcoesDisponiveis[i]){
            var opcao = document.querySelector(divsCardapio[i]);
-           menu.style.display = "none";
-           textoOpcoes.style.display = "none";
-           opcao.style.display = "block";
+           menu.classList.add("fade-out");
+           setTimeout(function(){
+              menu.style.display = "none";
+              menu.classList.remove("fade-out");
+           },500);
+           opcao.classList.add("fade-in");
+           setTimeout(function(){
+             opcao.style.display = "block";
+             opcao.classList.remove("fade-in");
+           },500);
        }
    }
+    botaoVoltar.forEach(function(botao){
+        botao.addEventListener("click",function(){
+             opcao.classList.add("fade-out");
+             setTimeout(function(){
+               opcao.style.display = "none";
+               opcao.classList.remove("fade-out");
+             },500);
+             menu.classList.add("fade-in");
+             setTimeout(function(){
+                menu.style.display = "block";
+                menu.classList.remove("fade-in");
+             },500);
+            console.log("Botao clicado");
+        });
+    });
 }
 menu.addEventListener("click",mostraOpcoesSabor);
-
-/*botaoVoltar.addEventListener("click",function(){
-    menu.style.display = "block";
-    opcao.style.display = "none";
-});*/
-
