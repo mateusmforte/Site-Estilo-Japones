400a179916be9124c9df50ae791d9dca6bf60a6a
Mateus Moura Forte <m-forte@hotmail.com>
2018-05-16 23:11:46 -0300

    Janela Login Adicionada na View

---
+++ view/assets/js/cardapio.js
@@ -0,0 +1,52 @@
+var menu = document.querySelector('#menu');
+var botaoVoltar = document.querySelectorAll(".botaoVoltar");
+function mostraOpcoesSabor(event){
+   var opcaoEscolhida = event.target.id;
+   var opcoesDisponiveis = ["opcao-temaki",
+                            "opcao-sashimi",
+                            "opcao-niguiri",
+                            "opcao-combos",
+                            "opcao-roll",
+                            "opcao-yakisoba",
+                            "opcao-variados",
+                            "opcao-bebidas"];
+    var divsCardapio = ["#temaki",
+                        "#sashimi",
+                        "#niguiri",
+                        "#combos",
+                        "#rollmaki",
+                        "#yakisoba",
+                        "#variados",
+                        "#bebidas"];
+   for(var i = 0; i<opcoesDisponiveis.length; i++){
+       if(opcaoEscolhida == opcoesDisponiveis[i]){
+           var opcao = document.querySelector(divsCardapio[i]);
+           menu.classList.add("fade-out");
+           setTimeout(function(){
+              menu.style.display = "none";
+              menu.classList.remove("fade-out");
+           },500);
+           opcao.classList.add("fade-in");
+           setTimeout(function(){
+             opcao.style.display = "block";
+             opcao.classList.remove("fade-in");
+           },500);
+       }
+   }
+    botaoVoltar.forEach(function(botao){
+        botao.addEventListener("click",function(){
+             opcao.classList.add("fade-out");
+             setTimeout(function(){
+               opcao.style.display = "none";
+               opcao.classList.remove("fade-out");
+             },500);
+             menu.classList.add("fade-in");
+             setTimeout(function(){
+                menu.style.display = "block";
+                menu.classList.remove("fade-in");
+             },500);
+            console.log("Botao clicado");
+        });
+    });
+}
+menu.addEventListener("click",mostraOpcoesSabor);
---
+++ view/assets/js/login.js
@@ -0,0 +1,6 @@
+var janelaLogin = $("#login");
+var loginLink = $("#loginLink");
+loginLino.onClick(function(){
+   loginLink.preventDefault();
+   janelaLogin.addClass("show");
+});
