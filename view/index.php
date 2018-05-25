98c19376b773ecb5fd80110f0b81725ca066eb2c
Rafael Figueiredo <rafaeldonfigueiredo@gmail.com>
2018-05-25 00:14:52 -0300

    Add conexão.php e login.php. Add index no form o action para login.

--- view/index.php
+++ view/index.php
@@ -62,7 +62,7 @@
         <div class="modal-dialog modal-lg" role="document">
             <div class="modal-content">
                 <div class="modal-body" style="padding:0px;"><a class="float-right btn-close" href="#"><i class="fa fa-close float-right"></i></a>
-                    <form method="post" id="cadastro-cliente">
+                    <form action = "login.php" method="post" id="cadastro-cliente">
                         <h2 class="text-center"><strong>Cadastre-se</strong></h2>
                         <div class="form-group"><input class="form-control" type="text" name="nome" placeholder="Nome Completo" id="nome"></div>
                         <div class="form-group"><input class="form-control" type="text" name="cpf" placeholder="CPF" id="cpf"></div>
