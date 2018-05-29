e1747d59c5824ce8644264312a6f178242196b2e
Rafael Figueiredo <rafaeldonfigueiredo@gmail.com>
2018-05-28 22:07:24 -0300

    Criado as funcoes de logica-usuario e banco-usuario

---
+++ view/banco-usuario.php
@@ -0,0 +1,12 @@
+<?php
+require_once("conecta.php");
+
+function buscaUsuario($conexao, $email, $senha){
+    $email = mysqli_real_escape_string($conexao, $email);
+    $query = "select * from usuarios where email= '{$email}' and senha '{$senha}'" 
+    $resultado = mysqli_query($conexao, $query);
+    $usuario = mysqli_fetch_assoc($resultado);
+    return $usuario;
+}
+
+
