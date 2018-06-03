2657b5f20c78dc712e7de30ee098102259dc3058
mateusmforte <m-forte@hotmail.com>
2018-06-03 17:30:59 -0300

    Correção bugs e adicionado arquivo produtos.js

--- view/assets/js/cardapio.js
+++ view/assets/js/cardapio.js
@@ -1,12 +1,15 @@
 var qtdAdd = $(".qtd-add");
 var qtdRemove = $(".qtd-remove");
+var produto = $(".qtd-produto");
+console.log(produto);
 
 qtdAdd.click(adicionaQtd);
 qtdRemove.click(removeQtd);
 function adicionaQtd(){
-    var qtd = parseInt($(".qtd-produto").val());
+    var qtd = parseInt(produto.val());
     qtdAdiciona = qtd + 1;
     $(".qtd-produto").val(qtdAdiciona);
+   
 }
 function removeQtd(){
     var qtd = parseInt($(".qtd-produto").val());
