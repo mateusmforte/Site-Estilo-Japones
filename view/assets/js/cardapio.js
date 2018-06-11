6ef8b6494d03dea1ba51724f1348ce02bf94f48e
Mateus Moura Forte <m-forte@hotmail.com>
2018-06-11 13:44:44 -0300

    Pequenas alterações e adição do arquivo carrinho.js

--- view/assets/js/cardapio.js
+++ view/assets/js/cardapio.js
@@ -1,22 +1,28 @@
 var qtdAdd = $(".qtd-add");
 var qtdRemove = $(".qtd-remove");
-var produto = $(".qtd-produto");
-console.log(produto);
 
-qtdAdd.click(adicionaQtd);
-qtdRemove.click(removeQtd);
-function adicionaQtd(){
-    var qtd = parseInt(produto.val());
-    qtdAdiciona = qtd + 1;
-    $(".qtd-produto").val(qtdAdiciona);
-   
+
+$(qtdAdd).on('click', function(){
+  adicionaQtd( $(this) );
+});
+$(qtdRemove).on('click', function(){
+  removeQtd( $(this) );
+});
+
+
+function adicionaQtd(el){
+	var produto = el.closest(".quantidade").find(".qtd-produto");
+  var qtd = parseInt(produto.val());
+  qtdAdiciona = qtd + 1;
+	produto.val(qtdAdiciona);
 }
-function removeQtd(){
-    var qtd = parseInt($(".qtd-produto").val());
-    if(qtd == 0){
-        return false;
-    }else{
-         qtdRemove = qtd - 1;
-         $(".qtd-produto").val(qtdRemove);
-    }   
+function removeQtd(el){
+	var produto = el.closest(".quantidade").find(".qtd-produto");
+  var qtd = parseInt(produto.val());
+  if(qtd == 0){
+      return false;
+  }else{
+      qtdRemove = qtd - 1;
+      produto.val(qtdRemove);
+  }   
 }
---
+++ view/assets/js/carrinho.js
@@ -0,0 +1,21 @@
+var btnCarrinho = $(".btn-adicionar-carrinho");
+var countCarrinho = $("#qntd-carrinho");
+var carrinho = $("#btn-carrinho");
+
+btnCarrinho.on("click",function(){
+    adicionaProduto($(this));
+});
+
+function adicionaProduto(el){
+    var re = /\d[0-9]*,\d[0-9]*/g;
+    var nomeProduto = el.parent().find(".titulo-produto");
+    var qtdProduto = el.parent().find(".qtd-produto").val();
+    var precoProduto = el.parent().find(".preco-produto").text();
+    var precoProdutoExp = re.exec(precoProduto).toString();
+    console.log(nomeProduto.text());
+    console.log(qtdProduto);
+    if(qtdProduto>0){
+        var totalProduto = (parseFloat(precoProdutoExp.replace(",",".")) * qtdProduto);
+        console.log(totalProduto);
+    }
+}
