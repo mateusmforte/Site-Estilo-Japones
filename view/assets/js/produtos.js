fbaf540887cde7135b46c6f67627b901adba89b2
mateusmforte <m-forte@hotmail.com>
2018-06-13 18:49:15 -0300

    Criando produtos dinamicamente com JS

--- view/assets/js/produtos.js
+++ view/assets/js/produtos.js
@@ -1,18 +1,12 @@
-$(function(){
+$(document).ready(function(){
     var produtos = $(".produtos").find(".card-group");
-    var teste = $("<h1></h1>").text("TESTE");
-    $.prepend(teste);
-    if(produtos){
-        console.log("achou");
-        console.log(produtos);
-    }
     produtos.prepend(criarProduto());
 });
 
 
 function criarProduto(){
-    var cardProduto = $("<div>").addClass("card flex-wrap item-cardapio produto"); //Cria o card 
-    var cardProdutoBody = $("<div>").addClass("card-body d-flex flex-column align-itens-center");//cria o corpo do card
+    var cardProduto = $("<div></div>").addClass("card flex-wrap item-cardapio produto"); //Cria o card 
+    var cardProdutoBody = $("<div></div>").addClass("card-body d-flex flex-column align-itens-center");//cria o corpo do card
 
     cardProdutoBody.append(insereImagemProduto());
     cardProdutoBody.append(insereTituloProduto());
@@ -22,22 +16,36 @@ function criarProduto(){
     cardProdutoBody.append(insereBtnAdicionarCarrinho());
 
     cardProduto.append(cardProdutoBody);
+    console.log(cardProduto);
+    console.log(cardProdutoBody);
+    console.log(insereImagemProduto());
+    console.log(insereTituloProduto());
+    console.log(inserePrecoProduto());
+    console.log(insereDescricaoProduto());
+    console.log(insereQuantidadeProduto());
+    console.log(insereBtnAdicionarCarrinho());
+    return cardProduto;
+   
 }
 function insereImagemProduto(){
     var img = $("<img>").addClass("img-fluid imagem-produto");//Tag img
     img.attr("src","assets/img/aea6de9cbaee9d2704dcf81f4a194991-754x394.jpg"); //!!! Fazer com que a imagem se altere através do banco!!!
+    return img;
 }
 function insereTituloProduto(){
-    var tituloProduto = $("<h4>").addClass("card-title titulo-produto");
+    var tituloProduto = $("<h4>").addClass("card-title titulo-produto center");
     tituloProduto.text("Titulo");
+    return  tituloProduto;
 }
 function inserePrecoProduto(){
-    var precoProduto = $("<h5>").addClass("preco-produto");
+    var precoProduto = $("<h5>").addClass("preco-produto center");
     precoProduto.text("R$0,00");
+    return precoProduto;
 }
 function insereDescricaoProduto(){
     var descricaoProduto = $("<p>").addClass("card-text descricao-produto");
     descricaoProduto.text("Nullam id dolor id nibh ultricies vehicula ut id elit. Cras justo odio, dapibus ac facilisis in, egestas eget quam. Donec id elit non mi porta gravida at eget metus.");
+    return descricaoProduto;
 }
 function insereQuantidadeProduto(){
     var divQtd = $("<div>").addClass("d-flex flex-column quantidade-produto");
@@ -56,11 +64,13 @@ function insereQuantidadeProduto(){
     qtdController.append(btnAdicionarQtd);
 
     divQtd.append(qtdController);
+    return divQtd;
 }
 function insereBtnAdicionarCarrinho(){
     var btnAdicionarCarrinho = $("<button>").addClass("btn btn-outline-danger btn-block btn-adicionar-carrinho");
     btnAdicionarCarrinho.attr("type","button");
     btnAdicionarCarrinho.text("Adicionar ao Carrinho");
+    return btnAdicionarCarrinho;
 }
 /*
 Produto
