a7392646e8fc69c80ed8351712af2f3b9f5f6ec1
Rafael Figueiredo <rafaeldonfigueiredo@gmail.com>
2018-06-19 22:30:38 -0300

    Criando a Classe Cliente

--- controller/cadastroUsuario.php
+++ controller/cadastroUsuario.php
@@ -3,7 +3,8 @@ require_once("banco-usuario.php");
 require_once("conecta.php");
 
 #Chamando a função de inserir novo cadastro. Checando se houve sucesso ou não.
-if(insereUsuario($conexao, $_POST['nome'], $_POST['cpf'], $_POST['endereco'],$_POST['telefone'], $_POST['email'], $_POST['senha'])){?>
+if(insereUsuario($conexao, $_POST['nome'], $_POST['cpf'], $_POST['endereco'],
+	$_POST['telefone'], $_POST['email'], $_POST['senha'])){?>
 	<p class="text-success">Usuário Cadastrado</p>
 <?php 
 } else {
--- model/Clientes.php
+++ model/Clientes.php
@@ -3,17 +3,76 @@
 Class Clientes {
 
     private $id;
-    private $usuario;
     private $senha;
     private $cpf;
     private $nome;
     private $endereco;
     private $email;
     private $telefone;
-    private $dtnasc;
 
-    function __construct($id, $usuario){
+    function __construct($senha,$cpf,$nome,$endereco,$email,$telefone){
+        $this->senha = $senha;
+        $this->cpf = $cpf;
+        $this->nome = $nome;
+        $this->endereco = $endereco;
+        $this->email = $email;
+        $this->telefone = $telefone;
+    }
 
+    function getID(){
+        return $this->id;
+    }
+
+    function setID($id){
+        $this->id = $id;
+    }
+
+    function getSenha(){
+        return $this->senha;
+    }
+
+    function setSenha($senha){
+        $this->senha = $senha;
+    }
+
+    function getCPF(){
+        return $this->cpf;
+    }
+
+    function setCPF($cpf){
+        $this->cpf = $cpf;
+    }
+
+    function getNome(){
+        return $this->nome;
+    }
+
+    function setNome($nome){
+        $this->nome = $nome;
+    }
+
+    function getEndereco(){
+        return $this->endereco;
+    }
+
+    function setEndereco($endereco){
+        $this->endereco = $endereco;
+    }
+
+    function getEmail(){
+        return $this->email;
+    }
+
+    function setEmail($email){
+        $this->email = $email;
+    }
+
+    function getTelefone(){
+        return $this->telefone;
+    }
+
+    function setTelefone($telefone){
+        $this->telefone = $telefone;
     }
 
     function alterarCadastro(){
