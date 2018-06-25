4ae4594c92682c3411ccd1ade378773a615ac9df
mateusmforte <m-forte@hotmail.com>
2018-06-25 15:34:04 -0300

    Categorias de produtos funcionando, alteração de id nas pills

--- view/assets/js/produtos.js
+++ view/assets/js/produtos.js
@@ -1,73 +1,97 @@
-var produtos = $(".produtos").find(".card-group");
+var categoriasExistentes = ["temakis"];
 
-produtos.prepend(criarProduto(null,"Camarão","R$5,90","Camarão Temaki"));
-produtos.prepend(criarProduto(null,"Salmão","R$10,90","Salmão Temaki"));
-produtos.prepend(criarProduto(null,"Atum","R$5,80","Atum Temaki"));
+produtosCategoria("temakis");
 
-var cardgroup = $(".card-group .card").length;
-console.log(cardgroup);
-function criarProduto(img,titulo,preco,descricao){
+var btnCategoria = ($("#opcoes-cardapio").find("a")).click(function(){
+    var idCategoria = $(this).attr("id").replace("btn-","");
+
+    if (categoriasExistentes.indexOf(idCategoria) < 0) {
+        categoriasExistentes.push(idCategoria);    
+        produtosCategoria(idCategoria);
+    }
+});
+function produtosCategoria(categoria) {
+    var cardapio = $("#cardapio-produtos>.produtos");    
+    var tabPanel = $("<div></div>");
+    tabPanel.addClass("tab-pane fade show active");
+    tabPanel.attr("role", "tabpanel");
+    var categoriasProd = ["temakis","niguiris","sashimis","rolls","yakisobas","combos","variados","sobremesas","bebidas"];
+    categoriasProd.forEach(function(i){
+        if(i == categoria){
+            tabPanel.attr("id", categoria);
+            for(var j = 0; j<2; j++){
+                tabPanel.append(criaGrupoProdutos());
+            }
+        }else{
+            return 0;
+        }
+    });
+    cardapio.append(tabPanel);
+    return cardapio;
+}
+function criaGrupoProdutos() {
+    var cardGroup = $("<div></div>");
+    cardGroup.addClass("card-group d-inline-flex align-items-center flex-wrap");
+    cardGroup.prepend(criarProduto(null, "Camarão", "R$5,90", "Camarão Temaki"));
+    cardGroup.prepend(criarProduto(null, "Salmão", "R$10,90", "Salmão Temaki"));
+    cardGroup.prepend(criarProduto(null, "Atum", "R$5,80", "Atum Temaki"));
+   
+    return cardGroup;
+}
+function criarProduto(img, titulo, preco, descricao) {
     var cardProduto = $("<div></div>").addClass("card flex-wrap item-cardapio produto"); //Cria o card 
     var cardProdutoBody = $("<div></div>").addClass("card-body d-flex flex-column align-itens-center");//cria o corpo do card
-
     cardProdutoBody.append(insereImagemProduto(img));
     cardProdutoBody.append(insereTituloProduto(titulo));
     cardProdutoBody.append(inserePrecoProduto(preco));
     cardProdutoBody.append(insereDescricaoProduto(descricao));
     cardProdutoBody.append(insereQuantidadeProduto());
     cardProdutoBody.append(insereBtnAdicionarCarrinho());
-
     cardProduto.append(cardProdutoBody);
     return cardProduto;
-   
 }
 
-function insereImagemProduto(img){
+function insereImagemProduto(img) {
     var img = $("<img>").addClass("img-fluid imagem-produto");//Tag img
-    img.attr("src","assets/img/aea6de9cbaee9d2704dcf81f4a194991-754x394.jpg"); //!!! Fazer com que a imagem se altere através do banco!!!
+    img.attr("src", "assets/img/aea6de9cbaee9d2704dcf81f4a194991-754x394.jpg"); //!!! Fazer com que a imagem se altere através do banco!!!
     return img;
 }
 
-function insereTituloProduto(titulo){
+function insereTituloProduto(titulo) {
     var tituloProduto = $("<h4>").addClass("card-title titulo-produto center");
     tituloProduto.text(titulo);
-    return  tituloProduto;
+    return tituloProduto;
 }
 
-function inserePrecoProduto(preco){
+function inserePrecoProduto(preco) {
     var precoProduto = $("<h5>").addClass("preco-produto center");
     precoProduto.text(preco);
     return precoProduto;
 }
-function insereDescricaoProduto(descricao){
+function insereDescricaoProduto(descricao) {
     var descricaoProduto = $("<p>").addClass("card-text descricao-produto");
     descricaoProduto.text(descricao);
     return descricaoProduto;
 }
 
-function insereQuantidadeProduto(){
+function insereQuantidadeProduto() {
     var divQtd = $("<div>").addClass("d-flex flex-column quantidade-produto");
     var txtSpanQtd = $("<span>").addClass("m-auto").text("Quantidade");
-
-
     var qtdController = $("<div>").addClass("m-auto quantidade");
-    var btnRemoveQtd = $("<button>").addClass("btn btn-danger qtd-botoes qtd-remove").attr("type","button").text("-");
-    var btnAdicionarQtd = $("<button>").addClass("btn btn-danger qtd-botoes qtd-add").attr("type","button").text("+");
-    var inputQtd = $("<input>").addClass("qtd-produto").attr("type","number").attr("value","0").attr("min","0");
-    
+    var btnRemoveQtd = $("<button>").addClass("btn btn-danger qtd-botoes qtd-remove").attr("type", "button").text("-");
+    var btnAdicionarQtd = $("<button>").addClass("btn btn-danger qtd-botoes qtd-add").attr("type", "button").text("+");
+    var inputQtd = $("<input>").addClass("qtd-produto").attr("type", "number").attr("value", "0").attr("min", "0");
     divQtd.append(txtSpanQtd);
-   
     qtdController.append(btnRemoveQtd);
     qtdController.append(inputQtd);
     qtdController.append(btnAdicionarQtd);
-
     divQtd.append(qtdController);
     return divQtd;
 }
 
-function insereBtnAdicionarCarrinho(){
+function insereBtnAdicionarCarrinho() {
     var btnAdicionarCarrinho = $("<button>").addClass("btn btn-outline-danger btn-block btn-adicionar-carrinho");
-    btnAdicionarCarrinho.attr("type","button");
+    btnAdicionarCarrinho.attr("type", "button");
     btnAdicionarCarrinho.text("Adicionar ao Carrinho");
     return btnAdicionarCarrinho;
 }
