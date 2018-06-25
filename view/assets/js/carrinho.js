8ef2827ace78a8e83a6f3fe1d32295f32fb8806c
mateusmforte <m-forte@hotmail.com>
2018-06-25 10:13:05 -0300

    Corrigido bug ao remover itens do carrinho e alteração da função removeSifrao

--- view/assets/js/carrinho.js
+++ view/assets/js/carrinho.js
@@ -1,6 +1,8 @@
 var carrinho = $("#tabela-pedido>table>tbody");
 var countCarrinho = $("#qntd-carrinho");
 var btnCarrinho = $("#btn-carrinho");
+var countItemAdicionado = 0;
+console.log(countItemAdicionado);
 $(document).ready(function(){
     var btnAdcionaCarrinho = $(".btn-adicionar-carrinho");
     btnAdcionaCarrinho.click(function(){
@@ -8,49 +10,72 @@ $(document).ready(function(){
         totalPedido(buscaItensPedido());
     });
 });
+function adicionaProduto(el){ //adiciona produto ao carrinho
+    var nomeProduto = el.parent().find(".titulo-produto");
+    var qtdProduto = el.parent().find(".qtd-produto").val();
+    var precoProduto = el.parent().find(".preco-produto").text();
+ 
+    alteraQtdItensCarrinho("add");
+    buildItemPedido(nomeProduto.text(),qtdProduto,precoProduto);
+}
 
-function buildItemPedido(nomeProduto,qtdProduto,precoProduto){
+function buildItemPedido(nomeProduto,qtdProduto,precoProduto){//cria a linha na tabela contendo as informações sobre o pedido
     var novoItem = $("<tr></tr>");
    
     var nomeItem = $("<td></td>").addClass("nome-pedido").text(nomeProduto);
     var qtdItem = $("<td></td>").addClass("qtd-pedido").text(qtdProduto);
     var qtdPrecoItem = $("<td></td>").addClass("qtd-preco-pedido").text(precoProduto);
     var removerItem = $("<td></td>").addClass("remover-item");
-    var botaoRemover = $("<span></span>").append("<i></i>").addClass("icon-close btn-remover-item");
+    var botaoRemover = $("<span></span>").append("<i></i>").addClass("icon-close btn-remover-item").attr("onclick","removeProduto(this)");
     removerItem.append(botaoRemover);
    
     novoItem.append(nomeItem,qtdItem,qtdPrecoItem,removerItem);
+    checkExiste(novoItem);
     carrinho.append(novoItem);
-    
     return carrinho;
 }
 
-function removeSifrao(texto){
-    var re =  /\d[0-9]*,\d[0-9]*/g;
-    var textoSemSifrao = re.exec(texto).toString();
-    return textoSemSifrao;
+function removeSifrao(texto){//função pra remover o Sifrão do texto recebido por padrão
+    texto = texto.replace("R$","").replace(".","").replace(",",".");
+    texto = parseFloat(texto).toFixed(2);
+    return texto;
 }
-
-function buscaItensPedido(){
+function checkExiste(item,carrinhoAtual){
+    $(carrinhoAtual).each(function(i){
+        console.log(carrinhoAtual);
+        console.log($(i).find(".nome-pedido"));
+        
+        if(item == $(i).find(".nome-pedido")){
+           return true;
+        } 
+    });
+}
+function buscaItensPedido(){ //Localiza todos os pedidos dentro do carrinho
     var itensCarrinho = $(carrinho).find("tr");
-    $("#lista-carrinho").hide();
-    $("#tabela-pedido").show();
+    
     var itens = [];
-   
+
     itensCarrinho.each(function(item){
+        var nomeProduto = $(this).find(".nome-pedido").text();        
         var qtdItem = $(this).find(".qtd-pedido").text();
         var precoItem = removeSifrao($(this).find(".qtd-preco-pedido").text());
         precoItem = precoItem.replace(",",".");
-        propriedadesItem = {
+        var propriedadesItem = {
+            nome : nomeProduto,
             preco : precoItem,
             qtd : qtdItem
-        };
-        itens.push(propriedadesItem);
+        };    
+        if(checkExiste(nomeProduto,itensCarrinho)){
+            console.log("Igual");
+        }else{
+            itens.push(propriedadesItem);
+        }   
     });
+   
     return itens;
 }
 
-function totalPedido(pedido){
+function totalPedido(pedido){//exibe o valor total dos itens no carrinho
     var totalPedido = $("#valor-total");
     var valores = [];
     var total = 0;
@@ -58,63 +83,48 @@ function totalPedido(pedido){
         var multiplica = parseFloat(pedido[i].preco) * parseInt(pedido[i].qtd);
         valores.push(multiplica);
         }
-        for (var j=0; j<valores.length; j++){
-            total = total + valores[j];
-        }
-        total = total.toFixed(2);
-        totalPedido.text("R$"+ total.toString().replace(".",","));
+    for (var j=0; j<valores.length; j++){
+        total = total + valores[j];
+    }
+    total = total.toFixed(2);
+    totalPedido.text("R$"+ total.toString().replace(".",","));
 }
 
-function adicionaProduto(el){ //adiciona produto ao carrinho
-    var nomeProduto = el.parent().find(".titulo-produto");
-    var qtdProduto = el.parent().find(".qtd-produto").val();
-    var precoProduto = el.parent().find(".preco-produto").text();
-    var precoProdutoExp = removeSifrao(precoProduto);
-    alteraQtdItensCarrinho("add");
-    buildItemPedido(nomeProduto.text(),qtdProduto,precoProduto);
-    $(".btn-remover-item").click(function(){
-        alteraQtdItensCarrinho("del");
-        removeProduto($(this));
-    });
-
-}
-
-function removeProduto(el){
-    var item = el.parent().parent();
+function removeProduto(el){// deleta produto do carrinho
+    var item = $(el).closest("tr");
     var itemPreco = item.find(".qtd-preco-pedido").text();
     var itemQtd = item.find(".qtd-pedido").text();
-    
+    alteraQtdItensCarrinho("del");
     subtraiValorTotal(itemPreco,itemQtd);
-    
     item.remove();
 }
 
-function subtraiValorTotal(itemPreco,itemQtd){
-    console.log(itemPreco,itemQtd);
-    precoItem = parseFloat(removeSifrao(itemPreco).replace(",",".")) * parseInt(itemQtd);
-    var total = parseFloat(removeSifrao($("#valor-total").text()).replace(",","."));
-    console.log(precoItem.toFixed(2));
-    console.log(total);
-    novoTotal = total - precoItem;
-    $("#valor-total").text("R$" + novoTotal.toFixed(2).toString().replace(".",","));
+function subtraiValorTotal(itemPreco,itemQtd){//diminui o valor total quando um item é removido
+    var precoItem = (removeSifrao(itemPreco) * itemQtd).toFixed(2);
+    var total = removeSifrao($("#valor-total").text());
+    var novoTotal = total - precoItem;
+    $("#valor-total").text("R$" + novoTotal.toFixed(2).replace(".",","));
 }
 
-var countItemAdicionado = 0;
-console.log(countItemAdicionado);
-function alteraQtdItensCarrinho(action){
+function alteraQtdItensCarrinho(action){// altera o contador (Badge) presente no icone de carrinho na barra de navegação
     if(action == "add"){
         if(countItemAdicionado >= 0)    
             countItemAdicionado ++;
-           // console.log(countItemAdicionado);
-            countCarrinho.text(countItemAdicionado.toString());
+        countCarrinho.text(countItemAdicionado.toString());
     }
     if(action == "del"){
-            if(countItemAdicionado > 0)    
+        if(countItemAdicionado > 0){
             countItemAdicionado -= 1;
-           // console.log(countItemAdicionado);
-            countCarrinho.text(countItemAdicionado.toString());
+        }  
+        countCarrinho.text(countItemAdicionado.toString());
+    }
+
+    if(countCarrinho.text() == "0"){
+        $("#tabela-pedido").hide();
+        $("#lista-carrinho").show();
     }
     else{
-        return 0;
+        $("#lista-carrinho").hide();
+        $("#tabela-pedido").show();
     }
 }
